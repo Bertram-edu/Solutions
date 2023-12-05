43ba5f7b957448984c87e06486386f43eed95ba9
Bertram-edu <63588@edu.campusvejle.dk>
2023-12-05 13:08:41 +0100

    S0610_morris_oop_exercise completed

--- 02_oop/python-files-from-opgaver/S0610_morris_oop_exercise.py
+++ 02_oop/python-files-from-opgaver/S0610_morris_oop_exercise.py
@@ -18,13 +18,13 @@ Når dit program er færdigt, skal du skubbe det til dit github-repository.
 Send derefter denne Teams-meddelelse til din lærer: <filename> færdig
 Fortsæt derefter med den næste fil."""
 
-
 import time
 
 time.sleep(1)
 
-#functions
-class miner:
+
+# functions
+class Miner:
 
     def __init__(self):
         self.turns = 0
@@ -35,14 +35,12 @@ class miner:
         self.gold = 0
 
     def __repr__(self):
-        return f"(turn: {self.turns}, sleepiness: {self.sleepiness}, thirst: {self.thirst}, hunger: {self.hunger}, whisky: {self.whisky}, gold: {self.gold})" # look like this: {'turn': 1000, 'sleepiness': 35, 'thirst': 74, 'hunger': 71, 'whisky': 0, 'gold': 1450}
-
+        return f"(turn: {self.turns}, sleepiness: {self.sleepiness}, thirst: {self.thirst}, hunger: {self.hunger}, whisky: {self.whisky}, gold: {self.gold})"  # look like this: {'turn': 1000, 'sleepiness': 35, 'thirst': 74, 'hunger': 71, 'whisky': 0, 'gold': 1450}
 
     def turn(self):
         self.turns += 1
         print()
         print(morris)
-
 
     def check(self):
         if self.sleepiness < 0:
@@ -61,13 +59,11 @@ class miner:
             self.gold = 0
         self.turn()
 
-
     def sleep(self):
         self.sleepiness -= 10
         self.thirst += 1
         self.hunger += 1
         self.check()
-
 
     def mine(self):
         self.sleepiness += 5
@@ -80,7 +76,7 @@ class miner:
     def eat(self):
         if self.gold > 2:
             self.sleepiness += 5
-            self.thirst -=5
+            self.thirst -= 5
             self.hunger -= 20
             self.gold -= 2
             self.check()
@@ -100,6 +96,7 @@ class miner:
                 print(f"morris doesn't have enough gold")
         else:
             print(f"morris can't hold any more whiskey")
+
     def drink(self):
         if morris.whisky > 0:
             self.sleepiness += 5
@@ -114,10 +111,9 @@ class miner:
         return self.sleepiness > 100 or self.thirst > 100 or self.hunger > 100
 
 
-
 # morrisdict = {"turn": 0, "sleepiness": 0, "thirst": 0, "hunger": 0, "whisky": 0, "gold": 0}  # dictionary
 
-morris = miner()
+morris = Miner()
 
 while not morris.dead() and morris.turns < 1000:
     while morris.sleepiness >= 20 and morris.turns < 1000:
