ebfd97e9a58ca29e1505ce4d6741a7da098f41fe
Bertram-RQ <bertramrq2@gmail.com>
2023-12-06 14:35:47 +0100

    S0630_random completed

---
+++ 02_oop/testing/random-testing.py
@@ -0,0 +1,33 @@
+
+
+
+import random
+random.seed(5)
+
+print(random.random())
+print(random.random())
+print(random.random())
+
+
+
+# Program to generate a random number
+# importing the random module
+import random
+
+print('Random number:', random.randint(0, 10))
+print('Random number:', random.randint(0, 50))
+print('Random number:', random.randint(0, 100))
+
+
+# Python program to illustrate choice() method
+import random
+
+# prints a random value from the list
+list1 = [8, 5, 6, 2, 0, 4]
+print('Random number from list:', random.choice(list1))
+
+# prints a random item from the string
+string = '9674271903'
+print('Random number from string:', random.choice(string))
+
+
