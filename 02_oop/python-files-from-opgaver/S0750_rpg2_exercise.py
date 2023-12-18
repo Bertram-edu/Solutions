fb2bff056aa045b7dd7e753b44d312ef903738fa
Bertram-RQ <bertramrq2@gmail.com>
2023-12-18 11:18:48 +0100

    wip

--- 02_oop/python-files-from-opgaver/S0750_rpg2_exercise.py
+++ 02_oop/python-files-from-opgaver/S0750_rpg2_exercise.py
@@ -29,3 +29,118 @@ Når dit program er færdigt, skal du skubbe det til dit github-repository.
 Send derefter denne Teams-besked til din lærer: <filename> done
 Fortsæt derefter med den næste fil."""
 
+
+import time
+import random
+
+time.sleep(1)
+
+
+class Character:
+    def __init__(self, name, health, attackpower):
+        self.name = name
+        self.max_health = health
+        self._current_health = health
+        self.attackpower = attackpower
+
+    def __repr__(self):
+        return f"name: ({self.name}) max health: ({self.max_health}) current health: ({self._current_health}) attack power: ({self.attackpower})"
+
+    def hit(self, other):
+        print()
+        print(f"{self.name} has hit {other.name} dealing {self.attackpower} damage")
+        print()
+        other.get_hit(self.attackpower)
+
+    def get_hit(self, attackpower):
+        self._current_health -= attackpower
+
+    def get_healed(self, healpower):
+        if self.max_health > self._current_health:
+            self._current_health += healpower
+        else:
+            print(f"{self.name} has reached their max_health of {self.max_health}")
+            self._current_health = self.max_health
+            print(f"{self.name}'s health has been set to their max health of {self.max_health}")
+
+    def failed_healed(self, healpower):
+        self._current_health -= healpower
+
+    def get_spellcasted(self, magicpower):
+        self._current_health -= magicpower
+
+
+class Healer(Character):
+
+    def __init__(self, name, health, healpower):
+        super().__init__(name, health, 0)
+        self.name = name
+        self.max_health = health
+        self._current_health = health
+        self.healpower = healpower
+        self.attackpower = 0
+
+    def heal(self, other):
+        temp = random.randint(1, 10)
+        if temp <= 7:
+            print()
+            print(f"{self.name} has healed {other.name} with a healpower of {self.healpower} {other.name} now has {other._current_health + self.healpower} health")
+            print()
+            other.get_healed(self.healpower)
+        elif temp == 8:
+            print()
+            print(f"{self.name} has failed so badly to heal {other.name} that they dealt {self.healpower} damage making {other.name} now has {other._current_health - self.healpower} health")
+            print()
+        else:
+            print()
+            print(f"{self.name} has failed to heal {other.name}")
+            print()
+
+
+class Wizard(Character):
+
+    def __init__(self, name, health, magicpower):
+        super().__init__(name, health, 0)
+        self.name = name
+        self.max_health = health
+        self._current_health = health
+        self.magicpower = magicpower
+        self.attackpower = 0
+
+    def spell(self, other):
+        temp = random.randint(1, 10)
+        if temp <= 7:
+            print()
+            print(f"{self.name} has used spell on {other.name} with a magic power of {self.magicpower} {other.name} now has {other._current_health - self.magicpower} health")
+            print()
+            other.get_spellcasted(self.magicpower)
+        elif temp == 9:
+            print()
+            print(f"{self.name} has missed {other.name} and hit themself dealing {self.magicpower} damage to themself their new health is {self._current_health - self.magicpower}")
+            print()
+            self.get_spellcasted(self.magicpower)
+        else:
+            print()
+            print(f"{self.name} has failed to use spell on {other.name}")
+            print()
+
+
+
+
+hero1 = Character("Bozeto", 100, 20)
+hero2 = Character("Andananda", 110, 24)
+hero3 = Healer("DoctorX", 75, 15)
+hero4 = Wizard("old man", 73, 20)
+print(hero1)
+print(hero2)
+print(hero3)
+hero1.hit(hero2)
+print(hero2)
+hero3.heal(hero2)
+print(hero2)
+
+hero4.spell(hero2)
+
+
+print(hero4)
+print(hero2)
