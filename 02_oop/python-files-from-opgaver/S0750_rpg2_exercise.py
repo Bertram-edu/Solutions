4668f837d60bddb4964b743bab945c5c2cd693f4
Bertram-edu <63588@edu.campusvejle.dk>
2023-12-19 12:12:10 +0100

    work in progress

--- 02_oop/python-files-from-opgaver/S0750_rpg2_exercise.py
+++ 02_oop/python-files-from-opgaver/S0750_rpg2_exercise.py
@@ -36,7 +36,7 @@ import random
 time.sleep(1)
 
 
-class Character:
+class Character: # can hit someone or fail and hit themself
     def __init__(self, name, health, attackpower):
         self.name = name
         self.max_health = health
@@ -47,10 +47,28 @@ class Character:
         return f"name: ({self.name}) max health: ({self.max_health}) current health: ({self._current_health}) attack power: ({self.attackpower})"
 
     def hit(self, other):
-        print()
-        print(f"{self.name} has hit {other.name} dealing {self.attackpower} damage")
-        print()
-        other.get_hit(self.attackpower)
+        if self.death_check():
+            temp = random.randint(1, 20)
+            if temp <= 17:
+                print()
+                print(f"{self.name} has hit {other.name} dealing {self.attackpower} damage")
+                print()
+                other.get_hit(self.attackpower)
+            elif temp == 19:
+                print()
+                print(f"{self.name} tried to hit {other.name} in the face but missed hitting themself in the face dealing {self.attackpower} damage to themself {self.name} now has {self._current_health - self.attackpower}")
+                print()
+                self.get_hit(self.attackpower)
+            else:
+                print()
+                print(f"{self.name} failed to hit {other.name}")
+                print()
+        else:
+            print()
+            print(f"{self.name} is dead and cannot do this")
+            print()
+    def death_check(self):
+        return self._current_health > 0
 
     def get_hit(self, attackpower):
         self._current_health -= attackpower
@@ -69,8 +87,20 @@ class Character:
     def get_spellcasted(self, magicpower):
         self._current_health -= magicpower
 
+    def get_light_hit(self, attackpower):
+        self._current_health -= attackpower
 
-class Healer(Character):
+    def get_double_light_hit(self, attackpower):
+        self._current_health -= attackpower * 2
+
+    def get_heavy_hit(self, attackpower):
+        self._current_health -= attackpower
+
+    def heavy_miss(self, attackpower):
+        self._current_health -= attackpower
+
+
+class Healer(Character): # Healer can try to heal someone but has the chance to hurt them instead
 
     def __init__(self, name, health, healpower):
         super().__init__(name, health, 0)
@@ -81,23 +111,28 @@ class Healer(Character):
         self.attackpower = 0
 
     def heal(self, other):
-        temp = random.randint(1, 10)
-        if temp <= 7:
-            print()
-            print(f"{self.name} has healed {other.name} with a healpower of {self.healpower} {other.name} now has {other._current_health + self.healpower} health")
-            print()
-            other.get_healed(self.healpower)
-        elif temp == 8:
-            print()
-            print(f"{self.name} has failed so badly to heal {other.name} that they dealt {self.healpower} damage making {other.name} now has {other._current_health - self.healpower} health")
-            print()
+        if self.death_check():
+            temp = random.randint(1, 10)
+            if temp <= 7:
+                print()
+                print(f"{self.name} has healed {other.name} with a healpower of {self.healpower} {other.name} now has {other._current_health + self.healpower} health")
+                print()
+                other.get_healed(self.healpower)
+            elif temp == 8:
+                print()
+                print(f"{self.name} has failed so badly to heal {other.name} that they dealt {self.healpower} damage making {other.name} now have {other._current_health - self.healpower} health")
+                print()
+                other.failed_healed(self.healpower)
+            else:
+                print()
+                print(f"{self.name} has failed to heal {other.name}")
+                print()
         else:
             print()
-            print(f"{self.name} has failed to heal {other.name}")
+            print(f"{self.name} is dead and cannot do this")
             print()
 
-
-class Wizard(Character):
+class Wizard(Character): # Wizard can cast spells dealing the attackpower defined when making the character they can also miss hitting themself dealing them damage instead
 
     def __init__(self, name, health, magicpower):
         super().__init__(name, health, 0)
@@ -108,21 +143,79 @@ class Wizard(Character):
         self.attackpower = 0
 
     def spell(self, other):
-        temp = random.randint(1, 10)
-        if temp <= 7:
-            print()
-            print(f"{self.name} has used spell on {other.name} with a magic power of {self.magicpower} {other.name} now has {other._current_health - self.magicpower} health")
-            print()
-            other.get_spellcasted(self.magicpower)
-        elif temp == 9:
-            print()
-            print(f"{self.name} has missed {other.name} and hit themself dealing {self.magicpower} damage to themself their new health is {self._current_health - self.magicpower}")
-            print()
-            self.get_spellcasted(self.magicpower)
+        if self.death_check():
+            temp = random.randint(1, 10)
+            if temp <= 7:
+                print()
+                print(f"{self.name} has used spell on {other.name} with a magic power of {self.magicpower} {other.name} now has {other._current_health - self.magicpower} health")
+                print()
+                other.get_spellcasted(self.magicpower)
+            elif temp == 9:
+                print()
+                print(f"{self.name} has missed {other.name} and hit themself dealing {self.magicpower} damage to themself their new health is {self._current_health - self.magicpower}")
+                print()
+                self.get_spellcasted(self.magicpower)
+            else:
+                print()
+                print(f"{self.name} has failed to use spell on {other.name}")
+                print()
         else:
             print()
-            print(f"{self.name} has failed to use spell on {other.name}")
+            print(f"{self.name} is dead and cannot do this")
             print()
+class Warrior(Character): # Warrior has light attack and heavy attack light attack will hit with the attackpower that's defined when making the character if lucky enough can cause double damage meanwhile the heavy attack has double the amount of attackpower by default but has less luck in success if unlucky enough they can miss hitting their foot dealing half their attack power to themself
+
+    def __init__(self, name, health, attackpower):
+        super().__init__(self, name, health)
+        self.name = name
+        self.max_health = health
+        self._current_health = health
+        self.attackpower = attackpower
+
+    def light_attack(self, other):
+        if self.death_check():
+            temp = random.randint(1, 10)
+            if temp <= 7:
+                print()
+                print(f"{self.name} has used light attack dealing {self.attackpower} damage to {other.name} {other.name} now has {other._current_health - self.attackpower} health")
+                print()
+                other.get_light_hit(self.attackpower)
+            elif temp == 8:
+                print()
+                print(f"{self.name} hit {other.name} so hard they dealt double damage of {self.attackpower * 2} {other.name} now has {other._current_health - (self.attackpower * 2) } health")
+                print()
+            else:
+                print()
+                print(f"{self.name} failed to hit {other.name} with light attack")
+                print()
+        else:
+            print()
+            print(f"{self.name} is dead and cannot do this")
+            print()
+
+    def heavy_attack(self, other):
+        if self.death_check():
+            heavy_attackpower = (self.attackpower * 2)
+            temp = random.randint(1, 10)
+            if temp <= 4:
+                print()
+                print(f"{self.name} has used heavy attack dealing {heavy_attackpower} damage to {other.name} {other.name} now has {other._current_health - heavy_attackpower} health")
+                print()
+                other.get_heavy_hit(heavy_attackpower)
+            elif temp == 7:
+                print()
+                print(f"{self.name} has missed {other.name} hitting themself in the foot {self.name} now has {self._current_health - int(self.attackpower / 2)} health")
+                print()
+                self.heavy_miss(int(self.attackpower / 2))
+            else:
+                print()
+                print(f"{self.name} has failed to hit {other.name} with heavy attack")
+                print()
+        else:
+            print()
+            print(f"{self.name} is dead and cannot do this")
+            print()
+
 
 
 
@@ -130,7 +223,8 @@ class Wizard(Character):
 hero1 = Character("Bozeto", 100, 20)
 hero2 = Character("Andananda", 110, 24)
 hero3 = Healer("DoctorX", 75, 15)
-hero4 = Wizard("old man", 73, 20)
+hero4 = Wizard("wise old man", 73, 20)
+hero5 = Warrior("Firestorm", 80, 20)
 print(hero1)
 print(hero2)
 print(hero3)
@@ -144,3 +238,33 @@ hero4.spell(hero2)
 
 print(hero4)
 print(hero2)
+
+print()
+print(hero1)
+print(hero5)
+
+hero5.light_attack(hero1)
+
+print(hero1)
+print(hero5)
+
+print()
+print()
+
+print(hero1)
+print(hero5)
+
+hero5.heavy_attack(hero1)
+
+print(hero1)
+print(hero5)
+
+print(hero1)
+
+hero5.heavy_attack(hero1)
+hero5.heavy_attack(hero1)
+hero5.heavy_attack(hero1)
+
+print(hero1)
+
+hero1.hit(hero5)
