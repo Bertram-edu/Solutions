caba87e9645749af68aed42c3b421c558995b391
Bertram-RQ <bertramrq2@gmail.com>
2024-01-24 10:41:30 +0100

    S1710_inventory_exercise completed

--- 03_fun/python-files-from-opgaver/S1710_inventory_exercise.py
+++ 03_fun/python-files-from-opgaver/S1710_inventory_exercise.py
@@ -28,6 +28,48 @@ i S1720_inventory_solution.py
 Når dit program er færdigt, skal du skubbe det til dit github-repository.
 Send derefter denne Teams-meddelelse til din lærer: <filename> færdig
 Fortsæt derefter med den næste fil."""
+import time
+
+
+def count_number(number):
+    counter = 0
+    for row in rows:
+        counter += row.count(number)
+    return counter
+
+def inventory(max_row):
+    for row_number in range(max_row):
+        row = []
+        rows.append(row)
+        column = 0
+        frequency = count_number(column)
+        while frequency > 0:
+            row.append(frequency)
+            column += 1
+            frequency = count_number(column)
+        row.append(0)
+        print(row)
+
+
+rows = []
+
+while True:
+    try:
+        inventoryamount = int(input("please chose a number: "))
+        break
+    except:
+        print("not number")
+
+time.sleep(1)
+
+start = time.time()
+
+inventory(inventoryamount)
+
+end = time.time()
+
+print(f"time to complete: {round(end - start, 4)}s")
+
 
 
 
