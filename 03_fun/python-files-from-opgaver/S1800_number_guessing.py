7ebb4653f4bf458d8ed2570fc5311584a0259b9b
Bertram-RQ <bertramrq2@gmail.com>
2024-01-24 14:14:08 +0100

    work in progress

--- 03_fun/python-files-from-opgaver/S1800_number_guessing.py
+++ 03_fun/python-files-from-opgaver/S1800_number_guessing.py
@@ -18,3 +18,34 @@ Når dit program er færdigt, skal du skubbe det til dit github-repository.
 Send derefter denne Teams-meddelelse til din lærer: <filename> færdig
 Fortsæt derefter med den næste fil."""
 
+import time
+import random
+
+guess_this_list = []
+
+for i in range(4):
+    randomnumber = random.randint(1, 9)
+    guess_this_list.append(randomnumber)
+
+temp = True
+
+while temp:
+    while True:
+        try:
+            number = int(input("guess the number: "))
+            break
+        except:
+            print("not a number")
+    if number > 9 or number < 1:
+        print(f"{number} is not in range of 1 to 9\nplease enter new number")
+        temp = True
+    else:
+        temp = False
+
+
+if number in guess_this_list:
+    print(f"{number} is in {guess_this_list}")
+else:
+    print(f"{number} is not in {guess_this_list}")
+
+
