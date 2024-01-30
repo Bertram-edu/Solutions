a0eb2c5be66758407e649e9c3a2f3e7d49990b4e
Bertram-RQ <bertramrq2@gmail.com>
2024-01-30 14:37:16 +0100

    work in progress

--- 03_fun/python-files-from-opgaver/S1800_number_guessing.py
+++ 03_fun/python-files-from-opgaver/S1800_number_guessing.py
@@ -27,8 +27,8 @@ guess_this_list = []
 numberlist = []
 correctnumberlist = []
 
-#blackcoin = 0
-#whitecoin = 0
+# blackcoin = 0
+# whitecoin = 0
 
 for i in range(4):
     randomnumber = random.randint(1, 9)
@@ -36,30 +36,59 @@ for i in range(4):
         randomnumber = random.randint(1, 9)
     guess_this_list.append(randomnumber)
 
-guess_this_list.clear()
-guess_this_list = [1, 1, 1, 1]
 
-temp = 0
+# for option 1:
+print(f"\nsome info on the game:\ninfo 1: the goal of the game is to guess the correct 4 digit number combination that is chosen at random in the least amount of guesses\ninfo 2: when you get a number that is in the list but not at the exact position you will get a whitecoin\ninfo 3: when you get a number that is in the list and is at the correct position you will get a blackcoin\ninfo 4: each attempt you will get to see what the current guess/attempt is worth\ninfo 5: when you've guessed the correct number you will get to see how many attempts it took\n\nthe rules are:\nrule 1: when chosing the number the number has to be 4 numbers without any spaces\n")
+
+# for option 2:
+# print(f"\nsome info on the game:\ninfo 1: the goal of the game is to guess the correct 4 digit number combination that is chosen at random in the least amount of guesses\ninfo 2: when you get a number that is in the list but not at the exact position you will get a whitecoin\ninfo 3: when you get a number that is in the list and is at the correct position you will get a blackcoin\ninfo 4: each attempt you will get to see what the current guess/attempt is worth\ninfo 5: when you've guessed the correct number you will get to see how many attempts it took\n\nthe rules are:\nrule 1: when chosing the number you will have to put in each number individually for a 4 number sequence\n")
 
 
-# new way without having to type each number individually
-while numberlist != guess_this_list and temp < 9999:
+attempts = 1
+
+# option 1 the way to do it without having to type each number individually
+while numberlist != guess_this_list:
     number = "0"
     numberlist.clear()
+    correctnumberlist.clear()
+    blackcoin = 0
+    whitecoin = 0
     while len(str(number)) != 4:
-        number = int(input("testing chose da number: "))
-        if len(str(number)) != 4:
-            print(f"{number} is not four numbers long\n")
+        try:
+            number = int(input("guess the number: "))
+            if len(str(number)) != 4:
+                print(f"{number} is not four numbers long\n")
+        except:
+            print("numbers only")
     for i in str(number):
         numberlist.append(int(i))
     print(numberlist)
-print(temp)
+    for i, currentnum in zip(range(4), numberlist):
+        if currentnum in guess_this_list and currentnum == guess_this_list[i]:
+            print(f"{currentnum} is correct number and position")
+            correctnumberlist.append(currentnum)
+            blackcoin += 1
+        elif currentnum in guess_this_list:
+            print(f"{currentnum} is in list but not at that position")
+            correctnumberlist.append(currentnum)
+            whitecoin += 1
+        else:
+            print(f"{currentnum} is not in list")
+    for index, k in zip(range(len(correctnumberlist)), correctnumberlist):
+        if correctnumberlist.count(k) > 1:
+            correctnumberlist.pop(index)
+
+    print(f"every number you got correct not in the correct order{correctnumberlist}")
+    print(f"\nattempt: {attempts} was worth {blackcoin} blackcoins and {whitecoin} whitecoins")
+    attempts += 1
+
+print(f"\nyou got it right in: {attempts - 1} attempts\n")
+print(f"\nthe list: {guess_this_list} your guess: {numberlist}\n")
 
 
-# works but you have to put each number individually
 
 # attempts = 1
-#
+# option 2 works but you have to put each number individually
 # while numberlist != guess_this_list:
 #     whitecoin = 0
 #     blackcoin = 0
