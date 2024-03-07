26ad94ae0117c6db49417b32591246325ee5630d
Bertram-edu <63588@edu.campusvejle.dk>
2024-03-07 10:00:46 +0100

    work in progress

--- 30_danskcargo/danskcargo/danskcargo_gui.py
+++ 30_danskcargo/danskcargo/danskcargo_gui.py
@@ -53,6 +53,20 @@ tree_scroll_container.grid(row=0, column=1, padx=0, pady=pady, sticky="ns")
 tree_container = ttk.Treeview(tree_frame_container, yscrollcommand=tree_scroll_container.set, selectmode="browse")
 tree_container.grid(row=0, column=0, padx=0, pady=pady)
 tree_scroll_container.config(command=tree_container.yview)
+
+# Define the data table's formatting and content
+tree_container["columns"] = ("id", "weight", "destination")
+tree_container.column("#0", width=0, stretch=tk.NO)
+tree_container.column("id", anchor=tk.E, width=40)
+tree_container.column("weight", anchor=tk.E, width=80)
+tree_container.column("destination", anchor=tk.W, width=200)
+tree_container.heading("#0", text="", anchor=tk.W)
+tree_container.heading("id", text="Id", anchor=tk.CENTER)
+tree_container.heading("weight", text="Weight", anchor=tk.CENTER)
+tree_container.heading("destination", text="Destination", anchor=tk.CENTER)
+tree_container.tag_configure("oddrow", background=oddrow)
+tree_container.tag_configure("evenrow", background=evenrow)
+
 # endregion container widgets
 
 # region main program
