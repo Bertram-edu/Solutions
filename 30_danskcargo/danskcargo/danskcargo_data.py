f7dc2d888e7817fab164e373b7f86ccd7fd9a306
Bertram-edu <63588@edu.campusvejle.dk>
2024-03-12 11:14:33 +0100

    work in progress

--- 30_danskcargo/danskcargo/danskcargo_data.py
+++ 30_danskcargo/danskcargo/danskcargo_data.py
@@ -1,6 +1,8 @@
 from sqlalchemy.orm import declarative_base
-from sqlalchemy import Column
-from sqlalchemy import String, Integer
+from sqlalchemy import Column, ForeignKey
+from sqlalchemy import String, Integer, Date
+from dateutil import parser
+from tkinter import messagebox
 
 
 Base = declarative_base()
@@ -15,7 +17,7 @@ class Container(Base):
     def __repr__(self):
         return f"Container({self.id=:4}     {self.weight=:5}    {self.destination=})"
 
-    def convert_to_tuple(self): # Convert Container to tuple
+    def convert_to_tuple(self):  # Convert Container to tuple
         return self.id, self.weight, self.destination
 
     def valid(self):
@@ -29,3 +31,68 @@ class Container(Base):
     def convert_from_tuple(tuple_):
         container = Container(id=tuple_[0], weight=tuple_[1], destination=tuple_[2])
         return container
+
+
+class Aircraft(Base):
+    __tablename__ = "aircraft"
+    id = Column(Integer, primary_key=True)
+    max_cargo_weight = Column(Integer)
+    registration = Column(String)
+
+    def __repr__(self):
+        return f"Aircraft({self.id=:4}     {self.max_cargo_weight=:5}    {self.registration=})"
+
+    def convert_to_tuple(self):  # Convert Container to tuple
+        return self.id, self.max_cargo_weight, self.registration
+
+    def valid(self):
+        try:
+            value = int(self.max_cargo_weight)
+        except ValueError:
+            return False
+        return value >= 0
+
+    @staticmethod
+    def convert_from_tuple(tuple_):
+        try:
+            max_cargo_weight = int(tuple[1])
+            if max_cargo_weight < 0:
+                messagebox.showwarning("", "max cargo weight must not be negative")
+            else:
+                aircraft = Aircraft(id=tuple_[0], max_cargo_weight=tuple_[1], registration=tuple_[2])
+                return aircraft
+        except:
+            messagebox.showwarning("", "entires could not be converted to Aircraft")
+
+
+class Transport(Base):
+    __tablename__ = "transport"
+    id = Column(Integer, primary_key=True)
+    date = Column(Date)
+    container_id = Column(Integer, ForeignKey("transport.container_id"), nullable=False)
+    aircraft_id = Column(Integer, ForeignKey("transport.aircraft_id"), nullable=False)
+
+    def __repr__(self):
+        return f"Aircraft({self.id=:4}     {self.date=:5}    {self.container_id=}    {self.aircraft_id=})"
+
+    def convert_to_tuple(self):  # Convert Container to tuple
+        return self.id, self.date, self.container_id, self.aircraft_id
+
+    def valid(self):
+        try:
+            value = int(self.container_id)
+        except ValueError:
+            return False
+        return value >= 0
+
+    @staticmethod
+    def convert_from_tuple(tuple_):
+        try:
+            date = parser.parse(tuple_[1])
+            container_id = int(tuple_[2])
+            aircraft_id = int(tuple_[3])
+            transport = Transport(id=tuple_[0], date=date, container_id=container_id, aircraft_id=aircraft_id)
+            return transport
+        except:
+            messagebox.showwarning("", "entries could not be converted to Transport")
+
